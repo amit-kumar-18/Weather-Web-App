4585e1ca5f8516ecb1e659ba71791c218b9ede5c
Amit Kumar <amitrawal316@gmail.com>
2023-04-04 18:57:35 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Weather Web App
+
+![Design preview for the Weather Web App](./assets/design/desktop%20design.png)
+
+## Welcome! 👋
+
+Thanks for checking out this repository.
+
+**This project is build with HTML, CSS, JavaScript, CSS framework Bulma and Axios library for HTTP requests.**
+
+## Getting Started
+
+This project was scaffolded with Vite. In the project directory, you can run:
+
+```sh
+npm install && npm run dev
+```
+
+To installs all the project dependencies.
+
+Open [http://localhost:5173](http://localhost:5173) to view it in your browser.
+
+## The Project
+
+This project is a weather web app which displays weather of every possible locations.
+
+We can search for locations to get their weather report with consists of weather conditions, temperature (min/max), humidity, and timezone.
+
+It also contains weather report for next 7 days.
+
+I have used Bulma Framework with CSS for this project.
+
+Users should be able to:
+
+- View weather report of every possible location.
+- Get 'Invalid location' notification for invalid or unavailable locations.
+
+## Deploying your project
+
+There are many ways to host your project for free. Recommend hosts are:
+
+- [GitHub Pages](https://pages.github.com/)
+- [Vercel](https://vercel.com/)
+- [Netlify](https://www.netlify.com/)
+
+**Have fun playing around!** 🚀
---
+++ main.js
@@ -0,0 +1,93 @@
+import axios from 'axios'
+
+// DOM imports
+const weatherImg = document.getElementById('weather-img')
+const weatherIcon = document.getElementById('weather-icon')
+const weatherCondition = document.getElementById('weather-conditon')
+const cityName = document.querySelector('.title')
+const temperature = document.querySelector('.subtitle')
+const dateTime = document.getElementById('date-time')
+const minTemp = document.getElementById('min-temp')
+const maxTemp = document.getElementById('max-temp')
+const description = document.getElementById('description')
+const searchBox = document.getElementById('search-box')
+const searchBtn = document.getElementById('search-btn')
+const timezone = document.getElementById('timezone')
+const dew = document.getElementById('dew')
+const humidity = document.getElementById('humidity')
+const feelsLike = document.getElementById('feels-like')
+const nextDays = document.querySelectorAll('.next-day')
+const nextIcon = document.querySelectorAll('.next-icon')
+const nextTemp = document.querySelectorAll('.next-temp')
+const notification = document.querySelector('.invalid')
+const deleteNotification = document.querySelector('.delete')
+const API_KEY = import.meta.env.VITE_API_KEY
+
+// Utility functions
+const getWeather = async (city = 'delhi') => {
+  try {
+    const URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=${API_KEY}`
+    const response = await axios.get(URL)
+    const data = response.data
+    setDetails(data)
+  } catch (error) {
+    if (error.response.status === 400) {
+      invalidLocation()
+    }
+  }
+}
+
+function setDetails(data) {
+  const iconImg = data.days[0].icon
+  cityName.textContent = data.resolvedAddress
+  temperature.textContent = `${data.days[0].temp} ℃`
+  dateTime.textContent = new Date(data.days[0].datetime).toDateString()
+  minTemp.textContent = `Min : ${data.days[0].tempmax} ℃`
+  maxTemp.textContent = `Max : ${data.days[0].tempmin} ℃`
+  description.textContent = data.days[0].description
+  weatherImg.src = `/assets/images/${iconImg}.jpg`
+  weatherImg.alt = data.days[0].conditions
+  weatherIcon.src = `/assets/weather-icons/${iconImg}.svg`
+  weatherIcon.alt = data.days[0].conditions
+  weatherCondition.textContent = data.days[0].conditions
+  timezone.textContent = data.timezone
+  dew.textContent = `${data.days[0].dew} ℃`
+  humidity.textContent = `${data.days[0].humidity} g.m-3`
+  feelsLike.textContent = `${data.days[0].feelslike} ℃`
+
+  nextDays.forEach((day, num) => {
+    day.textContent = new Date(data.days[num + 1].datetime).toLocaleDateString()
+  })
+
+  nextIcon.forEach((day, num) => {
+    day.src = `/assets/weather-icons/${data.days[num + 1].icon}.svg`
+    day.alt = data.days[num + 1].conditions
+  })
+
+  nextTemp.forEach((day, num) => {
+    day.textContent = `${data.days[num + 1].temp} ℃`
+  })
+}
+
+function invalidLocation() {
+  notification.classList.add('active')
+  setTimeout(() => {
+    notification.classList.remove('active')
+  }, 1000)
+}
+
+// Event Listeners
+searchBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  const city = searchBox.value
+  if (city) getWeather(city)
+  setTimeout(() => {
+    searchBox.value = ''
+  }, 2000)
+})
+
+deleteNotification.addEventListener('click', () => {
+  notification.classList.remove('active')
+})
+
+getWeather()
